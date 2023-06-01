d489770790a15bcdd2838ced65245066831b2611
EC2 Default User <ec2-user@ip-172-31-46-239.ap-northeast-1.compute.internal>
2023-06-01 10:47:39 +0000

    cart_item_update

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,8 +3,15 @@ class Public::CartItemsController < ApplicationController
   def create
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
-    @cart_item.save
-    redirect_to cart_items_path
+    @cart_item_search = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+    if @cart_item_search
+      @cart_item_search.amount += params[:cart_item][:amount].to_i
+      @cart_item_search.save
+      redirect_to cart_items_path
+    else
+      @cart_item.save
+      redirect_to cart_items_path
+    end
   end
 
   def index
