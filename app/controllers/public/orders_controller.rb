651075f4f2e4d8d13b530d0a874c986a57e223c1
EC2 Default User <ec2-user@ip-172-31-46-239.ap-northeast-1.compute.internal>
2023-06-04 08:52:48 +0000

    order_comfirm

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,7 +2,9 @@ class Public::OrdersController < ApplicationController
 
   def new
     @order = Order.new
+    @customer = current_customer
   end
+
 
   def index
   end
@@ -12,7 +14,23 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new(order_params)
+    select_address = params[:order][:select_address]
+    if select_address == "0"
+      @order.shipping_postal_code = current_customer.postal_code
+      @order.shipping_address = current_customer.address
+      @order.shipping_name = current_customer.last_name + current_customer.first_name
+    elsif select_address == "2"
+    end
+    @cart_items = current_customer.cart_items.all
+    @order.postage = 800
+    @total = 0
+    #binding.pry
+  end
+
+  def create
+    @order = Order.new(order_params)
     binding.pry
+    @order.save
   end
 
   private
